3558327c77f0eb6044844ec3d0c7abe4add0dcf8
APN <alainnamangale@gmail.com>
2024-12-12 10:47:24 +0200

    Third commit

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,7 @@
+import { role } from "@/lib/data";
+import Image from "next/image";
+import Link from "next/link";
+
 const menuItems = [
   {
     title: "MENU",
@@ -26,12 +30,12 @@ const menuItems = [
         href: "/list/parents",
         visible: ["admin", "teacher"],
       },
-      {
-        icon: "/subject.png",
-        label: "Subjects",
-        href: "/list/subjects",
-        visible: ["admin"],
-      },
+      // {
+      //   icon: "/subject.png",
+      //   label: "Subjects",
+      //   href: "/list/subjects",
+      //   visible: ["admin"],
+      // },
       {
         icon: "/class.png",
         label: "Classes",
@@ -56,12 +60,12 @@ const menuItems = [
         href: "/list/assignments",
         visible: ["admin", "teacher", "student", "parent"],
       },
-      {
-        icon: "/result.png",
-        label: "Results",
-        href: "/list/results",
-        visible: ["admin", "teacher", "student", "parent"],
-      },
+      // {
+      //   icon: "/result.png",
+      //   label: "Results",
+      //   href: "/list/results",
+      //   visible: ["admin", "teacher", "student", "parent"],
+      // },
       {
         icon: "/attendance.png",
         label: "Attendance",
@@ -112,3 +116,27 @@ const menuItems = [
     ],
   },
 ];
+
+const Menu = () => {
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map((i)=>(
+        <div className="flex flex-col gap-2" key={i.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
+          {i.items.map(item=>{
+            if(item.visible.includes(role)) {
+              return (
+                <Link href={item.href} key={item.label} className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight">
+                  <Image src={item.icon} alt="" width={20} height={20}/>
+                  <span className="hidden lg:block">{item.label}</span>
+                </Link>
+              );
+            }
+          })}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Menu
